04b1267271faa619042df18dff74f18cae4035b6
Vinicius Rolim Barbosa <vinii.rbarbosa@gmail.com>
2023-11-08 11:46:35 -0400

    Inicializando Projeto.

---
+++ DesafioMeta/Entities/Context/DesafioDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioMeta.Entities.Context;
+public class DesafioDbContext: DbContext
+{
+    public DesafioDbContext(DbContextOptions<DesafioDbContext> options) : base(options)
+    {}
+
+    public DesafioDbContext() { }
+
+    public DbSet<Pessoa> Pessoas { get; set; }
+    public DbSet<Endereco> Enderecos { get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                                                   .SetBasePath(Directory
+                                                   .GetCurrentDirectory())
+                                                   .AddJsonFile("appsettings.json")
+                                                   .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
+
---
+++ DesafioMeta/Entities/Pessoa.cs
@@ -0,0 +1,11 @@
+﻿namespace DesafioMeta.Entities;
+public class Pessoa
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string? CPF { get; set; }
+    public string? Email { get; set; }
+    public string Nacionalidade { get;set; }
+    public string Genero { get; set; }
+}
